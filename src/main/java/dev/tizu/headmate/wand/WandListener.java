cb9efedc2b56793421bcdc1fbb51b91e53047167
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-06-10 18:18:18 +0200

    editor mode wand

--- src/main/java/dev/tizu/headmate/wand/WandListener.java
+++ src/main/java/dev/tizu/headmate/wand/WandListener.java
@@ -1,14 +1,15 @@
 package dev.tizu.headmate.wand;
 
-import org.bukkit.Color;
 import org.bukkit.Material;
-import org.bukkit.Particle;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import dev.tizu.headmate.editor.Editor;
+import dev.tizu.headmate.headmate.HeadmateStore;
+import dev.tizu.headmate.menu.MenuList;
 import dev.tizu.headmate.util.Locator;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -18,14 +19,36 @@ public class WandListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         var player = event.getPlayer();
-        if (player.getInventory().getItemInOffHand().getType() != Material.BREEZE_ROD
+        if (player.getInventory().getItemInMainHand().getType() != Material.BREEZE_ROD
                 || event.getAction() == Action.PHYSICAL)
             return;
 
         event.setCancelled(true);
 
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
-            handleRayClick(event);
+        switch (event.getAction()) {
+            case RIGHT_CLICK_BLOCK:
+                if (player.isSneaking() && HeadmateStore.has(event.getClickedBlock())) {
+                    handleListClick(event);
+                    return;
+                }
+            case RIGHT_CLICK_AIR:
+                if (player.isSneaking()) {
+                    // the above falls through - if you're not looking at a block & sneaking OR
+                    // the block you're looking at has no merged heads, this fires! very neat.
+                    player.sendActionBar(Component.text(
+                            "No merged heads here, click with head in offhand to create one!",
+                            NamedTextColor.RED));
+                    return;
+                }
+                handleRayClick(event);
+                break;
+        }
+    }
+
+    private void handleListClick(PlayerInteractEvent event) {
+        var player = event.getPlayer();
+        var block = event.getClickedBlock();
+        player.openInventory(new MenuList(block).getInventory());
     }
 
     private void handleRayClick(PlayerInteractEvent event) {
@@ -36,13 +59,13 @@ public class WandListener implements Listener {
 
         var head = Locator.lookingAt(player.getEyeLocation(), considerations);
         if (head == null) {
-            player.sendActionBar(Component.text("no results", NamedTextColor.RED));
+            player.sendActionBar(
+                    Component.text("Could not find head, shift-click to select manually", NamedTextColor.RED));
             return;
         }
 
-        var vec = Locator.centerOfHead(head);
-        var loc = new org.bukkit.Location(player.getWorld(), vec.x(), vec.y(), vec.z());
-        player.spawnParticle(Particle.DUST, loc, 10, new Particle.DustOptions(Color.BLUE, 3f));
+        var block = head.getWorld().getBlockAt(head.getLocation());
+        Editor.startEditing(player, block, head);
     }
 
 }
